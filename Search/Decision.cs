a9c3f131e77361fd87a1c4a8c0f1f7fb264c09e5
FabioBis <fabio.biselli.80@gmail.com>
2014-06-16 00:52:03 +0200

    Added static functions Min and Max.

--- Search/Decision.cs
+++ Search/Decision.cs
@@ -53,10 +53,34 @@ namespace SharpSearch
         {
             impl = obj;
         }
-        
+
         public Object GetImpl()
         {
             return impl;
+        }
+
+        public static int Max(int a, int b)
+        {
+            if (a >= b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        public static int Min(int a, int b)
+        {
+            if (a > b)
+            {
+                return b;
+            }
+            else
+            {
+                return a;
+            }
         }
     }
 
